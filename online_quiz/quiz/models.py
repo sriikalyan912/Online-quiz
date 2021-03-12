6664f7acbcce055d6d4440a1517674f4474059e6
sriikalyan912 <sriikalyan18@gmail.com>
2021-03-12 15:07:48 +0530

    Deleting old one

--- online_quiz/quiz/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Quiz(models.Model):
-    name = models.CharField(max_length=32, null=False)
-    age = models.IntegerField(default=21)
-    phone = models.BigIntegerField(null=True)
-    
